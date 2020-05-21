8ba7a5f2b713b81ce42594796e7aacf77681d33b
Stepan <lukachina.stepan@mail.com>
2020-05-22 00:51:42 +0300

    finishing navbar and started slider

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+let elMenu = document.querySelectorAll('.navMenuList');
+let elList = document.querySelector('.outList');
+
+for (let elem of elMenu) { 
+    elem.addEventListener('mouseenter', () =>  {
+		elList.style.display = 'block';
+	});
+	elem.addEventListener('mouseleave', () =>  {
+		elList.style.display = 'none';
+	});
+}
+
+elList.addEventListener('mouseenter', () =>  {
+		elList.style.display = 'block';
+	});
+
+
